d53024bfa23ab03c03b3a2f8596d9840ece2091e
seokwoo-ahn <answ2005@gmail.com>
2022-08-08 16:21:15 +0900

    [feat] ejs

---
+++ chapter 7/ext-module.ejs.js
@@ -0,0 +1,12 @@
+var ejs = require('ejs')
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function(request, response) {
+    fs.readFile('ejsPage.ejs', 'utf8', function (error, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end(ejs.render(data))
+    })
+}).listen(52273, function() {
+    console.log('server running at localhost:52273')
+})
