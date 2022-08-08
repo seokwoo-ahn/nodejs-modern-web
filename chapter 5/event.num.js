acdeed4b54d369e143be505358ac2da70003ea74
seokwoo-ahn <answ2005@gmail.com>
2022-08-08 09:41:22 +0900

    [feat] event listener num

---
+++ chapter 5/event.num.js
@@ -0,0 +1,19 @@
+// max event listener. default:10, infinite: 0
+process.setMaxListeners(20)
+
+// default event num is 10, error occur
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
+process.on('exit', function() {})
