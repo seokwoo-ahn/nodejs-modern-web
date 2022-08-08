3831b98174c21cc48818feda69f8e8ecf981f300
seokwoo-ahn <answ2005@gmail.com>
2022-08-08 11:20:49 +0900

    [feat] add events

--- chapter 6/http.server.js
+++ chapter 6/http.server.js
@@ -2,6 +2,18 @@ var http = require('http')
 
 var server = http.createServer()
 
+server.on('request', function () {
+    console.log('Request on')
+})
+
+server.on('connection', function () {
+    console.log('Connection on')
+})
+
+server.on('close', function () {
+    console.log('close on')
+})
+
 server.listen(52273, function () {
     console.log('server running at http://localhost:52273')
 })
