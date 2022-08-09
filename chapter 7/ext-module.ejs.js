5e737af04af9b7180b671e7f287be562e6ab61ce
seokwoo-ahn <answ2005@gmail.com>
2022-08-09 09:53:24 +0900

    [refactor] send data to ejs

--- chapter 7/ext-module.ejs.js
+++ chapter 7/ext-module.ejs.js
@@ -5,7 +5,10 @@ var fs = require('fs')
 http.createServer(function(request, response) {
     fs.readFile('ejsPage.ejs', 'utf8', function (error, data) {
         response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(ejs.render(data))
+        response.end(ejs.render(data, {
+            name: 'cranberry',
+            description: 'Hello ejs world'
+        }))
     })
 }).listen(52273, function() {
     console.log('server running at localhost:52273')
