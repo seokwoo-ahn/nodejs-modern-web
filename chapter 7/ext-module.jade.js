a9eee552591092056fe9b53ae07d1d55d4651518
seokwoo-ahn <answ2005@gmail.com>
2022-08-09 10:23:27 +0900

    [refactor] dynamic jade

--- chapter 7/ext-module.jade.js
+++ chapter 7/ext-module.jade.js
@@ -6,7 +6,10 @@ http.createServer(function (request, response) {
     fs.readFile('jadePage.jade', 'utf8', function(error, data) {
         var fn = jade.compile(data)
         response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(fn())
+        response.end(fn({
+            name: 'cranberry',
+            description: 'elden ring!'
+        }))
     })
 }).listen(52273, function() {
     console.log('server running at http://127.0.0.1:52273')
