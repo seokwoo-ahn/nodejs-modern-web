955db684086b8c2e560f7b35606164d9b057a7f3
seokwoo-ahn <answ2005@gmail.com>
2022-08-09 15:32:01 +0900

    [feat] express router

--- chapter 8/express.middleware.js
+++ chapter 8/express.middleware.js
@@ -7,9 +7,26 @@ var app = express()
 app.use(morgan('combined'))
 app.use(express.static(__dirname + '/static'))
 
-app.use(function (request, response) {
+app.get('/image', function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'})
     response.end('<img src="/image.jpg" width="100%"/>')
+})
+
+app.get('/', function (request, response) {
+    response.status(200).send('cranberry!')
+})
+
+app.get('/id/:id/name/:name', function (request, response) {
+    var id = request.params.id
+    var name = request.params.name
+    var data = []
+    data.push('id: ' + id)
+    data.push('name: ' + name)
+    response.status(200).send(data)
+})
+
+app.all('*', function (request, response){
+    response.status(404).send('error')
 })
 
 http.createServer(app).listen(52273, function() {
