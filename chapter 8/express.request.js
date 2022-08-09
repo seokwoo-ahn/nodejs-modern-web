f6c2cb5b656fa69533de3d159f4465dc74be70ec
seokwoo-ahn <answ2005@gmail.com>
2022-08-09 14:28:32 +0900

    [feat] request query

--- chapter 8/express.request.js
+++ chapter 8/express.request.js
@@ -9,13 +9,19 @@ app.use(function (request, response) {
     console.log(request.headers)
     console.log(agent)
 
+    var id = request.query.id
+    var userName = request.query.name
+    
+    console.log("id:",id)
+    console.log("user name:",userName)
+
     if (agent.toLowerCase().match(/chrome/)) {
         response.send("hello chrome")
     } else {
         response.send("please use chorme")
     }
-    
-    response.status(200).send()
+
+    // response.status(200).send()
 })
 
 http.createServer(app).listen(52273, function() {
