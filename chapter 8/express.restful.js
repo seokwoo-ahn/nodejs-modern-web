ccefd2a301b5b8381cc449b84051564e0cf64c16
seokwoo-ahn <answ2005@gmail.com>
2022-08-13 09:32:14 +0900

    [feat] dummy database

--- chapter 8/express.restful.js
+++ chapter 8/express.restful.js
@@ -4,7 +4,38 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var DummyDB = (function () {
+    var DummyDB = {}
+    var storage = {}
+    var count = 1
 
+    DummyDB.get = function (id) {
+        if (id) {
+            id == (typeof id == 'string') ? Number(id) : id
+
+            for(var i in storage) if(storage[i].id == id) {
+                return storage[i]
+            }
+        } else {
+            return storage
+        }
+    }
+
+    DummyDB.insert = function (data) {
+        data.id = count++
+        storage.push(data)
+        return data
+    }
+
+    DummyDB.remove = function (id) {
+        id = (typeof id == 'string') ? Number(id) : id
+        for(var i in storage) if(storage[i].id == id) {
+            storage.splice(i, 1)
+            return true
+        }
+        return false
+    }
+
+    return DummyDB
 }) ();
 
 var app = express()
