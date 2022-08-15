d7ad8a40b69fdc19cdc4ca78dd573ec8b401aede
seokwoo-ahn <answ2005@gmail.com>
2022-08-15 14:51:02 +0900

    [feat] socket server

---
+++ chapter 11/socketio.server.js
@@ -0,0 +1,17 @@
+var http = require('http')
+var fs = require('fs')
+var socketio = require('socket.io')
+
+var server = http.createServer(function (request, response) {
+    fs.readFile('HTMLPAge.html', function (error, data) {
+        response.writeHead(200, { 'Content-Type': 'text/html'})
+        response.end(data)
+    })
+}).listen(52273, function() {
+    console.log('server running at localhost:52273')
+})
+
+var io = socketio(server)
+io.on('connection', function (socket) {
+    console.log("socket connected")
+})
